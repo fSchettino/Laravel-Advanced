1261e0a657facfc15073f80ca5626bea697f6f34
Fabio Schettino <fabio.schettino.01@gmail.com>
2019-09-18 16:36:00 +0200

    polymorphic relationships

---
+++ 03-Polymorphic-relationships/app/Tag.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    protected $guarded = [];
+
+    public function posts()
+    {
+        return $this->morphByMany(Post::class, 'taggable');
+    }
+
+    public function videos()
+    {
+        return $this->morphByMany(Video::class, 'taggable');
+    }
+}
